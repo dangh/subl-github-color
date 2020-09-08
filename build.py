16d08ed343c1272ff4b32bcaf5f7fc1be2020a44
Dang <dang@travelstop.com>
2020-09-09 01:56:35 +0800

    OneDarkPro: fix indent guide color, simplifier color syntax, avoid unnecessary build in dev mode

--- build.py
+++ build.py
@@ -9,6 +9,8 @@ from pathlib import Path
 def var_value(value):
 	value = str(value)
 	value = re.sub(r'(--[\w\.\-@/]+)', lambda r: 'var({})'.format(var_name(r.group())), value)
+	if re.match(r'var\([\w\-]+\)\s+\w+\(', value):
+		value = 'color({})'.format(value)
 	return value
 
 def var_name(var):
@@ -55,13 +57,12 @@ def build(data):
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
-	parser.add_argument('--dev', action='store_true')
+	parser.add_argument('colors', nargs='*', default=['./color_schemes/GitHub.yml', './color_schemes/OneDarkPro.yml'])
+	parser.add_argument('--dev', action='store_true', help='watch yml and generate color schemes on changes')
 	args = parser.parse_args()
 
-	color_schemes = ['GitHub', 'OneDarkPro']
-	for color_scheme in color_schemes:
-		src = 'color_schemes/{}.yml'.format(color_scheme)
-
+	for src in args.colors:
+		color_scheme = path.splitext(path.basename(src))[0]
 		dsts = ['color_schemes/{}.sublime-color-scheme'.format(color_scheme)]
 
 		if args.dev:
